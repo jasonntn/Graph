b015f5355ec6123b6a0a87858b34ad08b26b891f
aswquy100 <ntns1607@gmail.com>
2023-03-19 23:12:10 +0700

    add graph partitioning

--- graph.py
+++ graph.py
@@ -1,45 +1,47 @@
 class Vertex:
-    def __init__(self, id) -> None:
+    def __init__(self, id, neighbors: list = None) -> None:
         self.id = id
         self.neighbor = {}
+        if neighbors:
+            for neighbor in neighbors:
+                if isinstance(neighbor, tuple):
+                    self.add_neighbor(*neighbor)
+                else:
+                    self.add_neighbor(neighbor)
         
-    def add_neighbor(self, id, weight: int = 0):
+    def add_neighbor(self, id, weight: int = 1):
         self.neighbor[id] = weight
 
     def __str__(self):
-        return str(self.id) + ' <-> ' + str([x.id for x in self.neighbor])
-
-    @property
-    def neighbors(self):
-        return self.neighbor.keys()
+        return f"Vertex(id={self.id}, neighbor={list(self.neighbor.keys())})"
+    
+    def __eq__(self, __o: object) -> bool:
+        return self.id == __o.id
 
 
 class Graph:
-    def __init__(self) -> None:
+    def __init__(self, directed: bool = False) -> None:
         self.vertex = {}
+        self.directed = directed
 
-    def add_vertex(self, id):
-        vertex = Vertex(id)
-        self.vertex[id] = vertex
+    def add_vertex(self, vertex: Vertex):
+        self.vertex[vertex.id] = vertex
         return vertex
-
-    def get_vertex(self, id):
-        if id in self.vertex:
-            return self.vertex[id]
 
     def __contains__(self, id):
         return self.vertex[id]
 
-    def add_edge(self, id0, id1, weight: int = 0):
-        if id0 not in self.vertex:
-            self.add_vertex(id0)
-        if id1 not in self.vertex:
-            self.add_vertex(id1)
-        self.vertex[id0].add_neighbor(id1, weight)
-
-    @property
-    def vertices(self):
-        return self.vertex.keys()
+    def add_edge(self, edge: tuple):
+        if edge[0] not in self.vertex:
+            self.add_vertex(Vertex(edge[0]))
+        if edge[1] not in self.vertex:
+            self.add_vertex(Vertex(edge[1]))
+        if self.directed:
+            assert len(edge) == 3, "Directed graph's edge format: (vertex_id0, vertext_id1, weight)"
+            self.vertex[edge[0]].add_neighbor(edge[1], edge[2])
+        else:
+            self.vertex[edge[0]].add_neighbor(edge[1])
+            self.vertex[edge[1]].add_neighbor(edge[0])
 
     def __iter__(self):
         return iter(self.vertex.values())
@@ -62,7 +64,29 @@ class Graph:
             visited.append(s_id)
             for n_id in self.vertex[s_id].neighbors:
                 self.dfs(n_id, visited)
+            
+    
+    def get_partition(self):
+        subgraphs = []
+        current_subgraph = set()
+        for vertex in self:
+            if all([(v == vertex.id) or (v in vertex.neighbor.keys()) for v in current_subgraph]):
+                current_subgraph.add(vertex.id)
+            else:
+                subgraphs.append(current_subgraph)
+                current_subgraph = {vertex.id}
+        subgraphs.append(current_subgraph)
+        return subgraphs
 
+    def __str__(self) -> str:
+        conns = []
+        for s, e in self.vertex.items():
+            for n in e.neighbor.keys():
+                conns.append((s, n))
+        return f"Graph({conns})"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
 
 if __name__ == "__main__":
     directed_edges = [
@@ -102,16 +126,10 @@ if __name__ == "__main__":
         ("SAGE", "PALE"),
         ("SALT", "SAGE"),
     ]
-    test = [
-        (0, 1),
-        (0, 2),
-        (1, 2),
-        (2, 0),
-        (2, 3),
-        (3, 3)
-    ]
-    graph = Graph()
+    test = [("A", "B"), ("B", "C"), ("A", "C"), ("B", "E"), ("E", "D")]
+
+    graph = Graph(directed=False)
     for edge in test:
-        graph.add_edge(*edge)
-    graph.bfs(2)
-    graph.dfs(2, [])
+        graph.add_edge(edge)
+    print(graph)
+    print(graph.get_partition())
